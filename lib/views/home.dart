3530a814f04e016e7633f361a438141cfa367a72
RuhanPhelipe <ruhanphelipe>
2024-09-04 10:46:25 -0300

    minor change in Login Screen

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -53,98 +53,92 @@ class _HomeState extends State<Home> {
           SystemChannels.textInput.invokeMethod('TextInput.hide');
         },
         child: Scaffold(
-          body: Column(
-            children: [
-              Center(
-                child: Form(
-                  child: SafeArea(
-                      child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      InputTextField(
-                          textEditingController: _loginFieldController,
-                          label: 'Usuário',
-                          icon: Icons.account_circle,
-                          onFieldSubmitted: (value) {
-                            if (!_loginFieldKey.currentState!.validate()) {
-                              FocusScope.of(context)
-                                  .requestFocus(_loginFieldFocusNode);
-                            } else {
-                              _loginFieldFocusNode.unfocus();
-                              FocusScope.of(context)
-                                  .requestFocus(_passwordFieldFocusNode);
-                            }
-                          },
-                          onValidator: (value) {
-                            try {
-                              var isValid = TextFieldValidator(validators: [
-                                EmptyStrValidator(),
-                                MinLengthStrValidator(),
-                              ]).validations(value);
-
-                              if (!isValid) {
-                                return MessagesError.defaultError;
-                              }
-
-                              return null;
-                            } on Failure catch (e) {
-                              return e.msg;
-                            } catch (e) {
-                              return e.toString();
-                            }
-                          }),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      InputTextField(
-                          textEditingController: _passwordFieldController,
-                          label: 'Senha',
-                          icon: Icons.lock_person_rounded,
-                          onFieldSubmitted: (value) {
-                            if (!_passwordFieldKey.currentState!.validate()) {
-                              FocusScope.of(context)
-                                  .requestFocus(_passwordFieldFocusNode);
-                            } else {
-                              _passwordFieldFocusNode.unfocus();
-                              FocusScope.of(context)
-                                  .requestFocus(_signInButtonFocusNode);
-                            }
-                          },
-                          onValidator: (value) {
-                            try {
-                              var isValid = TextFieldValidator(validators: [
-                                EmptyStrValidator(),
-                                MinLengthStrValidator(),
-                              ]).validations(value);
-
-                              if (!isValid) {
-                                return MessagesError.defaultError;
-                              }
-
-                              return null;
-                            } on Failure catch (e) {
-                              return e.msg;
-                            } catch (e) {
-                              return e.toString();
-                            }
-                          }),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      ElevatedButton(
-                        focusNode: _signInButtonFocusNode,
-                        onPressed: () {},
-                        child: const Text('Entrar'),
-                      )
-                    ],
-                  )),
-                ),
-              ),
-              TextButton(
-                onPressed: () => context.goNamed(RoutesApp.signUp.name),
-                child: const Text('Cadastrar'),
-              ),
-            ],
+          body: Center(
+            child: Form(
+              child: SafeArea(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  InputTextField(
+                      textEditingController: _loginFieldController,
+                      label: 'Usuário',
+                      icon: Icons.account_circle,
+                      onFieldSubmitted: (value) {
+                        if (!_loginFieldKey.currentState!.validate()) {
+                          FocusScope.of(context)
+                              .requestFocus(_loginFieldFocusNode);
+                        } else {
+                          _loginFieldFocusNode.unfocus();
+                          FocusScope.of(context)
+                              .requestFocus(_passwordFieldFocusNode);
+                        }
+                      },
+                      onValidator: (value) {
+                        try {
+                          var isValid = TextFieldValidator(validators: [
+                            EmptyStrValidator(),
+                            MinLengthStrValidator(),
+                          ]).validations(value);
+                          if (!isValid) {
+                            return MessagesError.defaultError;
+                          }
+                          return null;
+                        } on Failure catch (e) {
+                          return e.msg;
+                        } catch (e) {
+                          return e.toString();
+                        }
+                      }),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  InputTextField(
+                      textEditingController: _passwordFieldController,
+                      label: 'Senha',
+                      icon: Icons.lock_person_rounded,
+                      onFieldSubmitted: (value) {
+                        if (!_passwordFieldKey.currentState!.validate()) {
+                          FocusScope.of(context)
+                              .requestFocus(_passwordFieldFocusNode);
+                        } else {
+                          _passwordFieldFocusNode.unfocus();
+                          FocusScope.of(context)
+                              .requestFocus(_signInButtonFocusNode);
+                        }
+                      },
+                      onValidator: (value) {
+                        try {
+                          var isValid = TextFieldValidator(validators: [
+                            EmptyStrValidator(),
+                            MinLengthStrValidator(),
+                          ]).validations(value);
+                          if (!isValid) {
+                            return MessagesError.defaultError;
+                          }
+                          return null;
+                        } on Failure catch (e) {
+                          return e.msg;
+                        } catch (e) {
+                          return e.toString();
+                        }
+                      }),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    focusNode: _signInButtonFocusNode,
+                    onPressed: () {},
+                    child: const Text('Entrar'),
+                  )
+                ],
+              )),
+            ),
+          ),
+          bottomNavigationBar: BottomAppBar(
+            child: TextButton(
+              onPressed: () => context.goNamed(RoutesApp.signUp.name),
+              child: const Text('Cadastrar'),
+            ),
           ),
         ));
   }
